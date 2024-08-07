f1e8a78faae536de1b569188863416786362a4e0
tarungantait <82888804+tarungantait@users.noreply.github.com>
2024-08-07 15:57:25 +0000

    new learn

---
+++ 01_Basics/01_Variable.js
@@ -0,0 +1,14 @@
+const accountId = 1087
+let email ="qwe@gmail.com"
+var accountpass = "abc123"
+accoutCity="Contai"
+
+//accountId=343 // not allowed for const
+
+/*
+preferable use let for functional block and block scope
+
+*/
+
+console.log(accountId);
+console.table([accountId,email,accountpass,accoutCity])
---
+++ 01_Basics/01_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict";  // for treat as new standerds
+
+//alert("Hello") we are using node JS not browser
+
+/*
+++Primitive Data Types
+number
+boolean
+bigint
+string
+NULL is a standalone Value like special type of blank ---Object
+undefined
+symbol -- unique
+
+*/
+let age= 'abcd'
+console.log(typeof null)
